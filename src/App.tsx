43ac5182a21727e4892dfd2a7a7440c177096133
Dave Goldsmith <davegoldsmith@hotmail.com>
2023-01-26 17:48:58 +0000

    Completed up to activity 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Header from './components/header';
-import Footer from './components/footer';
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { useState } from "react";
+import Cat from "./data/cat";
+import catData  from "./data/cat-data";
+import CatCard from "./components/cat-card";
+
 
 function App(): JSX.Element {
 
-	// JavaScript/TypeScript code can be inserted here!
+  const [cats, setCats] = useState<Array<Cat>>(catData);
+	const catCount = cats.length;
+	console.log("Cats tally = " + catCount);
 
-	return (
-		<>
-			<Navbar />
-			<Header />
+  return (
+    <>
+      <Navbar />
+      <Header catCount={catCount} />
 
-			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
-			</main>
+      <main>
+        <div className="cards__wrapper">
+          {cats.map((cat, index) => (
+            <CatCard
+							key={cat.id}
+							catObject={cat}
+							catIndex={index}
+              // name={cat.name}
+              // species={cat.species}
+              // favFoods={cat.favFoods}
+              // birthYear={cat.birthYear}
+            />
+          ))}
+        </div>
+      </main>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 }
 
 export default App;
