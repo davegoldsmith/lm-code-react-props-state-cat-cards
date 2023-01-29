0a782700e5bc1f7c22e7f760ddcd83b57813dfe8
Dave Goldsmith <davegoldsmith@hotmail.com>
2023-01-29 13:30:41 +0000

    Added reduce to comma separate favFoods

--- src/components/dog-card.tsx
+++ src/components/dog-card.tsx
@@ -13,7 +13,9 @@ const DogCard: React.FC<DogCardProps> = (props) => {
     <div className="card">
       <h3 className="card__text card__header">{name}</h3>
       <p className="card__text">{species}</p>
-      <p className="card__text">{favFoods}</p>
+      <p className="card__text">
+        {favFoods.reduce((prev, curr) => prev + ", " + curr)}
+      </p>
       <p className="card__text">{birthYear}</p>
     </div>
   );
